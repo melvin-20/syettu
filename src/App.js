406e85349a527f8ba9bf21355c5f79b9eab61533
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-22 14:45:12 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Home from './pages/Home'
 import './styles/app.scss'
+
+import Header from './components/Header'
 
 const App = () => {
     return (
-        <div>
-            <h1>Airbnb</h1>
+        <div classname="app">
+
+            {/* home */}
+            <Header />
+            <Home />
+
+                {/* Header */}
+
+                {/* Banner */}
+                    {/* Search */}
+
+                {/* Cards */}
+
+                {/* Footer */}
+            
+            {/* Search Page */}
         </div>
     )
 }
