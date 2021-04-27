ce6a4ea6a2a41ea762e71ddcafeec8212b53247b
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-28 01:49:05 +0530

    home page responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Banner from '../components/Banner'
-import Card from '../components/Card'
 import NearBy from '../components/NearBy'
 import LiveAnywhere from '../components/LiveAnywhere'
+import DiscoverPlaces from '../components/DiscoverPlaces'
 
 const Home = () => {
     return (
@@ -22,25 +22,9 @@ const Home = () => {
             {/* <div className="home__host">
 
             </div> */}
-            <div className="home__section">
-                <Card
-                    src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
-                    title="3 Bedroom Flat in Bournemouth"
-                    description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                    price="£130/night"
-                />
-                <Card
-                    src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-                    title="Penthouse in London"
-                    description="Enjoy the amazing sights of London with this stunning penthouse"
-                    price="£350/night"
-                />
-                <Card
-                    src="https://media.nomadicmatt.com/2018/apartment.jpg"
-                    title="1 Bedroom apartment"
-                    description="Superhost with great amenities and a fabolous shopping complex nearby"
-                    price="£70/night"
-                />
+            <div className="home__discover">
+                <h1>Discover Places </h1>
+                <DiscoverPlaces />
             </div>
         </div>
     )
